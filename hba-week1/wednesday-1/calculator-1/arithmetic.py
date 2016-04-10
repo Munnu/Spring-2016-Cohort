4712331341ebe08b22816b627f2d32842e3515cd
munnu <moniqueblake4@gmail.com>
2016-04-10 12:59:26 -0700

    Retry! Need to complete further study

---
+++ hba-week1/wednesday-1/calculator-1/arithmetic.py
@@ -0,0 +1,56 @@
+def add(num1, num2):
+    """Add two numbers."""
+    return num1 + num2
+
+def subtract(num1, num2):
+    """Subtract num2 from num1."""
+    return num1 - num2
+
+def multiply(num1, num2):
+    """Multiply num1 and num2."""
+    return num1 * num2
+
+def divide(num1, num2):
+    """Divide num1 by num2."""
+    # Need to turn at least argument to float for division to
+    # not be integer division
+    return float(num1) / float(num2) 
+
+def square(num1):
+    """Return square of num1."""
+    # Needs only one argument
+    return num1 * num1
+
+def cube(num1):
+    """Return cube of num1."""
+    # Needs only one argument
+    return num1 * num1 * num1
+
+
+def power(num1, num2):
+    """Raise num1 to the power of num2."""
+    return num1 ** num2  # ** = exponent operator
+
+def mod(num1, num2):
+    """Return the remainder of num1 divided by num2."""
+    return num1 % num2
+
+def divide(num1, num2):
+    """Divide num1 by num2."""
+    return num1 / num2
+
+def square(num1): 
+    """Return square of num1."""
+    return num1**2
+
+def cube(num1):
+    """Return cube of num1."""
+    return num1**3
+
+def power(num1, num2):
+    """Raise num1 to the power of num2."""
+    return num1**num2
+
+def mod(num1, num2):
+    """Return the remainder of num1 divided by num2."""
+    return num1 % num2
